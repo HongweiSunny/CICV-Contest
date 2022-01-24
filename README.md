78b54ebca68d46bd31214fada5be186ecabaed9f
HongweiSunny <2043500880@qq.com>
2022-01-24 22:29:17 +0800

    first creation

---
+++ README.md
@@ -0,0 +1,4 @@
+# CICV Contest 智能汽车算法攻关比赛
+
+Link: http://www.china-icv.cn/index.php/conference
+
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+cout << "dasdadsdasd";
+return 0；
+}
